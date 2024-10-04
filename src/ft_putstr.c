10216a0b5628949b03f0cc4ac8dfdf193258d690
Attila <kekesi.att@gmail.com>
2024-10-04 06:59:48 +0200

    add documentation to ft_putstr.c

--- src/ft_putstr.c
+++ src/ft_putstr.c
@@ -1,5 +1,15 @@
 #include "minitalk.h"
 
+/**
+ * @brief Outputs a string to the standard output (stdout).
+ * 
+ * This function takes a string as input and writes each character to the 
+ * standard output one by one, using the write() system call.
+ * 
+ * @param str Pointer to the null-terminated string to be printed.
+ * 
+ * @note This function does not append a newline at the end of the string.
+ */
 void	ft_putstr(char *str)
 {
 	int	i;
